1d88fff1283b2ae6b3b417c7fa07f56b0a3a0bf2
ggear <graham.gear@gmail.com>
2016-11-05 13:24:17 +0800

    Add Avro and HTTP test cases to explore impl/test options

--- asystem-anode/src/main/python/anode/test/test.py
+++ asystem-anode/src/main/python/anode/test/test.py
@@ -1,20 +1,12 @@
-import calendar
-import io
-import os
 import sys
-import time
-from uuid import getnode as get_mac
 
-import avro
-import avro.io
-import avro.schema
 from anode.anode import main
 from mock import patch
 from twisted.internet.task import Clock
 from twisted.trial.unittest import TestCase
 
 
-# noinspection PyUnresolvedReferences,PyMethodMayBeStatic
+# noinspection PyUnresolvedReferences
 class ANodeTest(TestCase):
     # noinspection PyPep8Naming
     # noinspection PyAttributeOutsideInit
@@ -36,27 +28,3 @@ class ANodeTest(TestCase):
     def test_main_quiet_long(self):
         with patch.object(sys, 'argv', ["anode", "--quiet"]):
             main(self.clock, lambda: self.tick_tock(1, 1))
-
-    def test_avro(self):
-        schema = avro.schema.parse(open(os.path.dirname(__file__) + "/../model/datum.avsc", "rb").read())
-        writer = io.BytesIO()
-        avro.io.DatumWriter(schema).write(
-            {
-                "anode": format(get_mac(), "x").decode("hex"),
-                "source": "fronius",
-                "metric": "energy.production.grid",
-                "type": "point",
-                "bin_width": 30,
-                "bin_unit": "s",
-                "bin_timestamp": calendar.timegm(time.gmtime()),
-                "data_timestamp": calendar.timegm(time.gmtime()) - 1,
-                "data_unit": "kW",
-                "data_value": 10
-            },
-            avro.io.BinaryEncoder(writer))
-        datum_bytes = writer.getvalue()
-        print("Serialised to [{}] bytes".format(len(datum_bytes)))
-        reader = avro.io.DatumReader(schema)
-        datum_dict = reader.read(avro.io.BinaryDecoder(io.BytesIO(datum_bytes)))
-        print("Deserialized to [{}] bytes".format(len(str(datum_dict))))
-        print(datum_dict['anode'])
---
+++ asystem-anode/src/main/python/anode/test/test_http.py
@@ -0,0 +1,77 @@
+from __future__ import print_function
+
+import treq
+from treq import text_content
+from treq.api import get
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+from twisted.trial.unittest import TestCase
+from twisted.web.client import HTTPConnectionPool
+
+TEST_URL = "http://10.0.1.203/solar_api/v1/GetPowerFlowRealtimeData.fcgi"
+TEST_URL_CODE = 200
+# noinspection PyPep8
+TEST_URL_TEXT = """{
+  "Head" : {
+    "RequestArguments" : {},
+    "Status" : {
+      "Code" : 0,
+      "Reason" : "",
+      "UserMessage" : ""
+    },
+    "Timestamp" : "2016-11-05T13:05:01+08:00"
+  },
+  "Body" : {
+    "Data" : {
+      "Site" : {
+        "Mode" : "meter",
+        "P_Grid" : -4630.06,
+        "P_Load" : -410.94,
+        "P_Akku" : null,
+        "P_PV" : 5340,
+        "E_Day" : 23547,
+        "E_Year" : 2245403.2,
+        "E_Total" : 2245411.2,
+        "Meter_Location" : "grid"
+      },
+      "Inverters" : {
+        "1" : {
+          "DT" : 99,
+          "P" : 5041
+        }
+      }
+    }
+  }
+}"""
+
+
+class ANodeTestHttp(TestCase):
+    def test_http(self):
+        get(TEST_URL, pool=HTTPConnectionPool(reactor, False)).addCallback(self.process_response)
+        reactor.run()
+
+    @inlineCallbacks
+    def process_response(self, response):
+        response_code = response.code
+        response_text = yield text_content(response)
+        print('Code: {}, Length: {}, Text:\n{}'.format(response_code, len(response_text), response_text))
+        reactor.stop()
+
+    def test_http_mock(self):
+        self.patch(treq, 'get', lambda url: MockResponse(url))
+        self.patch(treq, 'text_content', lambda response: response.text)
+        treq.get(TEST_URL).addCallback(lambda response: print(response))
+
+
+# noinspection PyPep8Naming
+class MockResponse:
+    def __init__(self, url):
+        self.url = url
+        self.code = TEST_URL_CODE
+        self.text = TEST_URL_TEXT
+
+    def __repr__(self):
+        return 'Code: {}, Length: {}, Text:\n{}'.format(self.code, len(self.text), self.text)
+
+    def addCallback(self, callback):
+        callback(self)
