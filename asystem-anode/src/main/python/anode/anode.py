d718ce1b1faba27f415db18925548550079261fe
ggear <graham.gear@gmail.com>
2016-11-12 14:55:34 +0800

    WebSocket, RESTful and Web server impl

--- asystem-anode/src/main/python/anode/anode.py
+++ asystem-anode/src/main/python/anode/anode.py
@@ -1,35 +1,116 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import logging.config
+import os
+import sys
+import urlparse
 from optparse import OptionParser
 
-from plugin import Plugin
+from autobahn.twisted.resource import WebSocketResource
+from autobahn.twisted.websocket import WebSocketServerFactory, WebSocketServerProtocol
+from klein import Klein
+from klein.resource import KleinResource
 from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
+from twisted.python import log
+from twisted.web.client import HTTPConnectionPool
+from twisted.web.server import Site
+from twisted.web.static import File
+
+from plugin import Plugin
+
+LOG_FORMAT = "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+WEB_PORT = 8080
 
 
 class ANode:
-    def __init__(self, clock, callback, options):
-        self.clock = clock
+    def __init__(self, main_reactor, callback, options):
+        self.main_reactor = main_reactor
         self.callback = callback
         self.options = options
         self.plugins = []
+        self.web_ws = WebWsFactory(u"ws://127.0.0.1:" + str(WEB_PORT), self)
+        self.web_ws.protocol = WebWs
+        self.web_rest = WebRest(self)
+        self.web_pool = HTTPConnectionPool(reactor, persistent=True)
 
     def plugin(self, plugin_name, plugin_config):
-        plugin = LoopingCall(Plugin.get(plugin_name, plugin_config).loop)
-        plugin.clock = self.clock
-        plugin.start(plugin_config['poll'])
+        plugin_deffered = LoopingCall(Plugin.get(plugin_name, plugin_config).poll)
+        plugin_deffered.clock = self.main_reactor
+        plugin_deffered.start(plugin_config["poll"])
         return plugin_name
 
     def start(self):
-        self.plugins.append(self.plugin("davis", {"quiet": self.options.quiet, "poll": 1}))
-        self.plugins.append(self.plugin("fronius", {"quiet": self.options.quiet, "poll": 1}))
-        self.plugins.append(self.plugin("netatmo", {"quiet": self.options.quiet, "poll": 1}))
-        self.plugins.append(self.plugin("publish", {"quiet": self.options.quiet, "poll": 1}))
-        self.plugins.append(self.plugin("callback", {"quiet": self.options.quiet, "poll": 1, "callback": self.callback}))
-        if hasattr(self.clock, 'run'):
-            self.clock.run()
+        if logging.getLogger().isEnabledFor(logging.INFO):
+            logging.getLogger().info("Starting service ...")
+
+        self.plugins.append(self.plugin("davis", {"poll": 1, "push": True, "pool": self.web_pool}))
+        self.plugins.append(self.plugin("fronius", {"poll": 1, "push": False, "pool": self.web_pool}))
+        self.plugins.append(self.plugin("netatmo", {"poll": 1, "push": True, "pool": self.web_pool}))
+        self.plugins.append(self.plugin("publish", {"poll": 2, "push": True, "pool": self.web_pool}))
+        self.plugins.append(self.plugin("callback", {"poll": 1, "callback": self.callback}))
+
+        web_root = File(os.path.dirname(__file__) + "/web")
+        web_root.putChild(b"push", WebSocketResource(self.web_ws))
+        web_root.putChild(u"pull", KleinResource(self.web_rest.server))
+
+        if self.main_reactor == reactor:
+            self.main_reactor.listenTCP(WEB_PORT, Site(web_root))
+            self.main_reactor.run()
+
+        return self
 
 
-def main(clock=reactor, callback=None):
+class WebWsFactory(WebSocketServerFactory):
+    def __init__(self, url, anode):
+        WebSocketServerFactory.__init__(self, url)
+        self.anode = anode
+
+
+# noinspection PyPep8Naming
+class WebWs(WebSocketServerProtocol):
+    def __init__(self):
+        super(WebWs, self).__init__()
+        self.datum_filter = None
+
+    def onConnect(self, request):
+        self.datum_filter = request.params
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            logging.getLogger().debug("WebSocket connection request [{}]".format(filter))
+
+    def onOpen(self):
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            logging.getLogger().debug("WebSocket connected")
+
+    def onClose(self, wasClean, code, reason):
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            logging.getLogger().debug("WebSocket connection closed [{0}]".format(reason))
+
+
+# noinspection PyPep8Naming
+class WebRest:
+    server = Klein()
+
+    def __init__(self, anode):
+        self.anode = anode
+
+    @server.route("/")
+    def onRequest(self, request):
+        datum_filter = urlparse.parse_qs(urlparse.urlparse(request.uri).query)
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            logging.getLogger().debug("RESTful request [{0}]".format(datum_filter))
+
+
+def main(main_reactor=reactor, callback=None):
     parser = OptionParser()
-    parser.add_option("-q", "--quiet", action="store_true", dest="quiet", default=False, help="suppress output to stdout")
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="noisy output to stdout")
+    parser.add_option("-q", "--quiet", action="store_true", dest="quiet", default=False, help="suppress all output to stdout")
     (options, args) = parser.parse_args()
-    ANode(clock, callback, options).start()
+    if not logging.getLogger().handlers:
+        logging_handler = logging.StreamHandler(sys.stdout)
+        logging_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+        logging.getLogger().addHandler(logging_handler)
+        log.PythonLoggingObserver(loggerName=logging.getLogger().name).start()
+    logging.getLogger().setLevel(logging.CRITICAL if options.quiet else (logging.DEBUG if options.verbose else logging.INFO))
+    return ANode(main_reactor, callback, options).start()
--- asystem-anode/src/main/python/anode/plugin/plugin.py
+++ asystem-anode/src/main/python/anode/plugin/plugin.py
@@ -1,36 +1,143 @@
 from __future__ import print_function
-from __future__ import print_function
 
 import abc
+import io
+import logging
+import os
+from collections import deque
+from decimal import Decimal
 from importlib import import_module
+from uuid import getnode as get_mac
+
+import avro
+import avro.io
+import avro.schema
+import avro.schema
+from avro.io import AvroTypeException
 
 import anode.plugin
 
 
 class Plugin(object):
-    __metaclass__ = abc.ABCMeta
-
-    def __init__(self, config):
-        self.config = config
-        if not self.config['quiet']:
-            print(str(type(self).__name__) + ":init")
-
+    # noinspection PyStatementEffect
     @abc.abstractmethod
-    def loop(self):
-        if not self.config['quiet']:
-            print(str(type(self).__name__) + ":loop")
+    def poll(self):
+        None
+
+    def datum_ping(self):
+        if logging.getLogger().isEnabledFor(logging.INFO):
+            logging.getLogger().info("Plugin [{}] pinged [{}] datums".format(self.name, 0))
+
+    def datum_pop(self):
+        datums_popped = 0
+        if logging.getLogger().isEnabledFor(logging.INFO):
+            logging.getLogger().info("Plugin [{}] dropped [{}] datums".format(self.name, self.datums_dropped))
+            logging.getLogger().info("Plugin [{}] pushed [{}] datums".format(self.name, self.datums_pushed))
+        if "push" in self.config and self.config["push"]:
+            for datum_metric in self.datums:
+                for datum_type in self.datums[datum_metric]:
+                    for datum_bins in self.datums[datum_metric][datum_type]:
+                        # noinspection PyCompatibility
+                        for i in xrange(len(self.datums[datum_metric][datum_type][datum_bins]["queue"])):
+                            datum_avro = self.datums[datum_metric][datum_type][datum_bins]["queue"].popleft()
+                            if logging.getLogger().isEnabledFor(logging.DEBUG):
+                                logging.getLogger().debug("Popped datum [{}]".format(self.datum_tostring(self.datum_deserialise(datum_avro))))
+                            # TDOD: push to QMTT broker, returning datums to left of deque if push fails
+                            datums_popped += 1
+        self.datums_pushed = 0
+        self.datums_dropped = 0
+        if logging.getLogger().isEnabledFor(logging.INFO):
+            logging.getLogger().info("Plugin [{}] popped [{}] datums".format(self.name, datums_popped))
+
+    def datum_push(self, data_metric, data_type, data_value, data_unit, data_scale, data_timestamp, bin_timestamp, bin_width, bin_unit):
+        datum_dict = {
+            "anode_id": self.mac_address,
+            "data_source": self.name,
+            "data_metric": data_metric,
+            "data_type": data_type,
+            "data_value": data_value,
+            "data_unit": data_unit,
+            "data_scale": data_scale,
+            "data_timestamp": data_timestamp,
+            "bin_timestamp": bin_timestamp,
+            "bin_width": bin_width,
+            "bin_unit": bin_unit
+        }
+        try:
+            datum_avro = self.datum_serialise(datum_dict)
+        except AvroTypeException as error:
+            if logging.getLogger().isEnabledFor(logging.ERROR):
+                logging.getLogger().error("Error serialising Avro object [{}]".format(error))
+            return
+        if datum_dict["data_metric"] not in self.datums:
+            self.datums[datum_dict["data_metric"]] = {}
+        if datum_dict["data_type"] not in self.datums[datum_dict["data_metric"]]:
+            self.datums[datum_dict["data_metric"]][datum_dict["data_type"]] = {}
+        if str(datum_dict["bin_width"]) + datum_dict["bin_unit"] not in self.datums[datum_dict["data_metric"]][datum_dict["data_type"]]:
+            self.datums[datum_dict["data_metric"]][datum_dict["data_type"]][str(datum_dict["bin_width"]) + datum_dict["bin_unit"]] = {"queue": deque()}
+        datums_deref = self.datums[datum_dict["data_metric"]][datum_dict["data_type"]][str(datum_dict["bin_width"]) + datum_dict["bin_unit"]]
+        if "last" not in datums_deref or datums_deref["last"]["data_value"] != datum_dict["data_value"] or datums_deref["last"]["data_unit"] != \
+                datum_dict["data_unit"] or datums_deref["last"]["data_scale"] != datum_dict["data_scale"]:
+            datums_deref["last"] = datum_dict
+            datums_deref["queue"].append(datum_avro)
+            self.datums_pushed += 1
+            if logging.getLogger().isEnabledFor(logging.DEBUG):
+                logging.getLogger().debug("Pushed datum [{}]".format(self.datum_tostring(datum_dict)))
+        else:
+            self.datums_dropped += 1
+            if logging.getLogger().isEnabledFor(logging.DEBUG):
+                logging.getLogger().debug("Dropped datum [{}]".format(self.datum_tostring(datum_dict)))
+
+    def datum_tostring(self, datum_dict):
+        return "{}.{}={}{}".format(
+            datum_dict["data_source"],
+            datum_dict["data_metric"],
+            int(self.datum_deserialise(self.datum_serialise(datum_dict))["data_value"]) / Decimal(datum_dict["data_scale"]),
+            datum_dict["data_unit"]
+        )
+
+    @staticmethod
+    def datum_serialise(datum_dict):
+        avro_writer = io.BytesIO()
+        avro.io.DatumWriter(DATUM_SCHEMA).write(datum_dict, avro.io.BinaryEncoder(avro_writer))
+        return avro_writer.getvalue()
+
+    @staticmethod
+    def datum_deserialise(datum_avro):
+        return avro.io.DatumReader(DATUM_SCHEMA).read(avro.io.BinaryDecoder(io.BytesIO(datum_avro)))
+
+    @staticmethod
+    def get_int_scaled(numeric, factor=1):
+        return 0 if numeric is None else int(numeric * factor)
 
     @staticmethod
     def get(module, config):
-        return getattr(import_module("anode.plugin") if hasattr(anode.plugin, module.title()) else import_module("anode.plugin." + module), module.title())(config)
+        plugin = getattr(import_module("anode.plugin") if hasattr(anode.plugin, module.title()) else import_module("anode.plugin." + module), module.title())(module, config)
+        if logging.getLogger().isEnabledFor(logging.INFO):
+            logging.getLogger().info("Plugin [{}] initialised".format(module))
+        return plugin
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, name, config):
+        self.name = name
+        self.config = config
+        self.mac_address = format(get_mac(), "x").decode("hex")
+        self.datums = {}
+        self.datums_pushed = 0
+        self.datums_dropped = 0
+
+
+DATUM_SCHEMA = avro.schema.parse(open(os.path.dirname(__file__) + "/../model/datum.avsc", "rb").read())
 
 
 class Publish(Plugin):
-    def loop(self):
-        super(self.__class__, self).loop()
+    # noinspection PyStatementEffect
+    def poll(self):
+        None
 
 
 class Callback(Plugin):
-    def loop(self):
-        if self.config["callback"] is not None:
+    def poll(self):
+        if "callback" in self.config and self.config["callback"] is not None:
             self.config["callback"]()
