f70f921760130d30fbbc624bfbe78ea18e313c1d
ggear <graham.gear@gmail.com>
2018-03-28 20:47:40 +0800

    Update generated files for asystem-10.000.0022

--- asystem-amodel/src/main/script/python/energyforecast_intraday.py
+++ asystem-amodel/src/main/script/python/energyforecast_intraday.py
@@ -249,7 +249,7 @@ def pipeline():
         dfnsa.plot(title="Energy Normalised/Standardised (Mean) - VETTED", legend=False)
 
     model_file = '/model/pickle/joblib/none/' \
-                 'amodel_version=10.000.0021' \
+                 'amodel_version=10.000.0022-SNAPSHOT' \
                  '/amodel_model=1000' \
                  '/model.pkl'
     local_model_file = local_model_path + model_file
