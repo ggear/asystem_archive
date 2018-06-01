fd51b3a74741d9dc031219852e3f315eb03dc7ad
ggear <graham.gear@gmail.com>
2018-06-01 16:50:00 +0800

    Resolve refactoring bug

--- asystem-amodel/src/main/python/energyforecast_interday.py
+++ asystem-amodel/src/main/python/energyforecast_interday.py
@@ -68,7 +68,7 @@ def execute(model=None, features=None,
     from sklearn.linear_model import RidgeCV
     from sklearn.linear_model import LassoCV
     from sklearn.feature_extraction import DictVectorizer
-    features = [
+    feature_columns = [
         'temperature',
         'rain_mm',
         'humidity_mbar',
@@ -76,7 +76,7 @@ def execute(model=None, features=None,
         'day_length_sec',
         'condition'
     ]
-    features_original = [
+    feature_columns_original = [
         'datum__bin__date',
         'energy__production__inverter',
         'temperature__forecast__glen_Dforrest',
@@ -86,7 +86,7 @@ def execute(model=None, features=None,
         'conditions__forecast__glen_Dforrest',
         'day_length_sec'
     ]
-    features_rename = {
+    feature_columns_rename = {
         'datum__bin__date': 'date',
         'energy__production__inverter': 'energy',
         'temperature__forecast__glen_Dforrest': 'temperature',
@@ -96,7 +96,7 @@ def execute(model=None, features=None,
         'conditions__forecast__glen_Dforrest': 'condition'
     }
     if labels:
-        return features, features_original, features_rename
+        return feature_columns, feature_columns_original, feature_columns_rename
     elif statistics:
         energy_label = 'energy' if 'energy' in features \
             else 'energy__production__inverter'
@@ -109,9 +109,9 @@ def execute(model=None, features=None,
         features_engineered['day_length_sec'] = \
             features_engineered['sun__outdoor__set'] \
             - features_engineered['sun__outdoor__rise']
-        features_engineered_renamed = features_engineered[features_original]
+        features_engineered_renamed = features_engineered[feature_columns_original]
         features_engineered_renamed = features_engineered_renamed \
-            .rename(columns=features_rename)
+            .rename(columns=feature_columns_rename)
 
         # TODO: Remove
         # features_engineered_renamed['rain_mm_poly'] = \
@@ -122,7 +122,7 @@ def execute(model=None, features=None,
     elif prediction:
         if type(features) is not np.ndarray:
             features = model['vectorizer'] \
-                .transform(features[features].to_dict(orient='record'))
+                .transform(features[feature_columns].to_dict(orient='record'))
         predictions = model['pipeline'].predict(features)
         if 'statistics' in model:
             predictions = predictions.clip(
--- asystem-amodel/src/main/script/python/energyforecast_interday.py
+++ asystem-amodel/src/main/script/python/energyforecast_interday.py
@@ -71,7 +71,7 @@ def execute(model=None, features=None,
     from sklearn.linear_model import RidgeCV
     from sklearn.linear_model import LassoCV
     from sklearn.feature_extraction import DictVectorizer
-    features = [
+    feature_columns = [
         'temperature',
         'rain_mm',
         'humidity_mbar',
@@ -79,7 +79,7 @@ def execute(model=None, features=None,
         'day_length_sec',
         'condition'
     ]
-    features_original = [
+    feature_columns_original = [
         'datum__bin__date',
         'energy__production__inverter',
         'temperature__forecast__glen_Dforrest',
@@ -89,7 +89,7 @@ def execute(model=None, features=None,
         'conditions__forecast__glen_Dforrest',
         'day_length_sec'
     ]
-    features_rename = {
+    feature_columns_rename = {
         'datum__bin__date': 'date',
         'energy__production__inverter': 'energy',
         'temperature__forecast__glen_Dforrest': 'temperature',
@@ -99,7 +99,7 @@ def execute(model=None, features=None,
         'conditions__forecast__glen_Dforrest': 'condition'
     }
     if labels:
-        return features, features_original, features_rename
+        return feature_columns, feature_columns_original, feature_columns_rename
     elif statistics:
         energy_label = 'energy' if 'energy' in features \
             else 'energy__production__inverter'
@@ -112,9 +112,9 @@ def execute(model=None, features=None,
         features_engineered['day_length_sec'] = \
             features_engineered['sun__outdoor__set'] \
             - features_engineered['sun__outdoor__rise']
-        features_engineered_renamed = features_engineered[features_original]
+        features_engineered_renamed = features_engineered[feature_columns_original]
         features_engineered_renamed = features_engineered_renamed \
-            .rename(columns=features_rename)
+            .rename(columns=feature_columns_rename)
 
         # TODO: Remove
         # features_engineered_renamed['rain_mm_poly'] = \
@@ -125,7 +125,7 @@ def execute(model=None, features=None,
     elif prediction:
         if type(features) is not np.ndarray:
             features = model['vectorizer'] \
-                .transform(features[features].to_dict(orient='record'))
+                .transform(features[feature_columns].to_dict(orient='record'))
         predictions = model['pipeline'].predict(features)
         if 'statistics' in model:
             predictions = predictions.clip(
