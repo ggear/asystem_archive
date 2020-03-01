5c5305195bccb1dca2a5a133d9fcb0da5d0f55f5
Graham Gear <graham.gear@gmail.com>
2020-03-01 08:12:52 +0800

    Update sensros

--- asystem-anode/src/main/python/anode/plugin/plugin.py
+++ asystem-anode/src/main/python/anode/plugin/plugin.py
@@ -404,7 +404,7 @@ class Plugin(object):
                             if datum_id not in PUBLISH_METADATA_CACHE:
                                 datum_metadata_topic = "{}/sensor/anode/{}/config".format(publish_push_metadata_topic, datum_id)
                                 datum_metadata = {
-                                    "unique_id": datum_id,
+                                    "unique_id": "GG " + datum_id,
                                     "name": datum_name,
                                     "value_template": "{{value_json.value}}",
                                     "unit_of_measurement": datum_dict_decoded["data_unit"],
